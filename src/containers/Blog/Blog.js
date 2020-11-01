2790250a3f07be5c2cf51b706866a1354eb39250
jthanawat <kla13thanawat@gmail.com>
2020-11-01 22:25:35 +0700

    fetch data to the screen

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,21 +6,29 @@ import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
 
 class Blog extends Component {
+  state = {
+    posts: [],
+  };
+
   componentDidMount() {
-    axios.get("https://jsonplaceholder.typicode.com/posts")
-    .then((res) => {
-        console.log(res)
+    // but trying to store them in a post constant won't work bcuz that get request happens asyncronously. it doesnt finish immedialy, it needs some time to go to the server and get the data.
+    // axios uses promises
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      this.setState({ posts: res.data });
+      console.log(res);
     });
+    // javascript doesn't wait for this to finish so if you're immediately calling this set state after get method, the data wont have been fetched yet.
+    // this.setState({})
   }
 
   render() {
+    const posts = this.state.posts.map((post) => {
+      return <Post key={post.id} title={post.title} />;
+    });
+
     return (
       <div>
-        <section className="Posts">
-          <Post />
-          <Post />
-          <Post />
-        </section>
+        <section className="Posts">{posts}</section>
         <section>
           <FullPost />
         </section>
