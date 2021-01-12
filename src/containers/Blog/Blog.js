b79ad121f44ab679841375a25d537c8d320d455f
jthanawat <kla13thanawat@gmail.com>
2021-01-12 13:15:47 +0700

    render components for routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 // import axios from "axios";
-import axios from '../../axios'
-import "./Blog.css";
-import Posts from './Posts/Posts'
 
+import "./Blog.css";
+import Posts from "./Posts/Posts";
+import { Route } from "react-router-dom";
 class Blog extends Component {
   render() {
     return (
@@ -11,13 +11,21 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/new-post">New Post</a></li>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/new-post">New Post</a>
+              </li>
             </ul>
           </nav>
         </header>
-        <Posts />
-        
+        {/* <Route path="/" exact render={()=> <h1>Home</h1>} />
+        <Route path="/" render={()=> <h1>Home 2</h1>} /> */}
+        <Route path="/" exact component={Posts} />
+
+        {/* <Posts /> */}
+
         {/* <section>
           <FullPost id={this.state.selectedPostId} />
         </section>
